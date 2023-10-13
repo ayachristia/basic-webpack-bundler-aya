44ad4361c8098b8025d76d1674f5c126a45fd1f1
Aya <ayachristia@gmail.com>
2023-10-13 11:07:05 +0200

    push uden dist

--- src/scripts/burgermenu.js
+++ src/scripts/burgermenu.js
@@ -1,5 +1,4 @@
 export default (function burgermenu() {
-<<<<<<< HEAD
     const button = document.querySelector(".menuButton")
     const menu = document.querySelector(".primaryMenu")
 
@@ -9,14 +8,3 @@ export default (function burgermenu() {
         menu.style.right = menu.style.right == "-19em" ? "0" : "-19em"
     }
 })()
-=======
-	const button = document.querySelector(".menuButton")
-	const menu = document.querySelector(".primaryMenu")
-
-	button.addEventListener("click", clickHandler)
-
-	function clickHandler() {
-		menu.style.right = menu.style.right == "-19em" ? "0" : "-19em"
-	}
-})()
->>>>>>> 847bd4a74e2cdc4b61a803037cd89424916d2d6c
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-<<<<<<< HEAD
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require('path')
@@ -64,62 +63,3 @@ module.exports = {
 		static: './dist',
 	}
 }
-=======
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const path = require("path")
-
-module.exports = {
-	//mode: process.env.environment == "production" ? "production" : "development",
-	mode: "production",
-	plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
-		template: path.resolve(__dirname, "src", "index.html")
-	})],
-	module: {
-		rules: [
-			{
-				test: /\.(s[ac]|c)ss$/i,
-				//test: path.resolve(__dirname, "src/scss/*"),
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", "postcss-loader"]
-			},{
-				test: /\.(png|jpe?g)$/i,
-				type: "asset",
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							name: "[name].[ext]",
-							outputPath: "images/"
-						}
-					}
-				]
-			}
-		]
-	},
-	optimization: {
-		minimizer: [
-			new ImageMinimizerPlugin({
-				minimizer: {
-					implementation: ImageMinimizerPlugin.sharpMinify,
-					options: {
-						encodeOptions: {
-							jpeg: {
-                // https://sharp.pixelplumbing.com/api-output#jpeg
-                quality: 20,
-              },
-							png: {
-								quality: 20,
-								compressionLevel: 9
-							}
-						}
-					}
-				}
-			})
-		]
-	},
-	devtool: "source-map",
-	devServer: {
-		static: "./dist"
-	}
-}
->>>>>>> 847bd4a74e2cdc4b61a803037cd89424916d2d6c
